4da5bf190f7e240f6694863b915a25d2843f267c
faujiiiahhh <framandaadelia@gmail.com>
2023-10-30 09:37:03 +0700

    Modifikasi 1

--- ArrayBilangan10.java
+++ ArrayBilangan10.java
@@ -2,16 +2,10 @@ import java.util.Scanner;
 
 public class ArrayBilangan10 {
     public static void main (String[] args){
-        int [] bil = new int [4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
-
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        int [] bil = {12367,5, 7, 2000000};
+        for (int i = 0; i< 4; i++){
+            System.out.println(bil[i]);
+        }
 
     }
 }
